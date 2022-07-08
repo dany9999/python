0d73aa78236dcb0192a273c263b2e1ba96ce6255
dany9999 <daniele99abbagale@gmail.com>
2022-07-08 11:16:46 +0200

    data

---
+++ Generating_data/mpl_squares.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+squares = [1, 4, 9, 16, 25] #x -> indici dell'array  Y-> valore assunto in quell'indice
+
+fig, ax = plt.subplots()
+ax.plot(squares, linewidth=3)
+
+#set chart title and label axes
+ax.set_title("Square Numbers", fontsize = 24)
+ax.set_xlabel("Value", fontsize = 14)
+ax.set_ylabel("Square of Value", fontsize = 14)
+
+# Set size of tick labels
+ax.tick_params(axis= 'both', labelsize = 14)
+
+#ax.plot(squares)
+
+plt.show()
